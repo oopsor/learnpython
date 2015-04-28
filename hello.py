8dc4b41f5a8966821c30376284cf0cec79a9caba
TOURISCAT <ireadcode@163.com>
2015-04-28 12:46:18 +0800

    6

--- hello.py
+++ hello.py
@@ -1,9 +1,47 @@
 #!/usr/bin/env python
 # coding=utf-8
-birth = int(raw_input('birth:'))
-if birth < 1990:
-	print '90前'
-elif birth < 2000:
-	print '90后'
-else:
-	print '00后'
+import functools
+
+def log(func):
+	@functools.wraps(func)
+	def wrapper(*args,**kw):
+		print 'begin call'
+		func(*args,**kw)
+		print 'end call'
+	return wrapper
+
+@log
+def now():
+	print '2015-04-13'
+
+now()
+
+import functools
+
+def log(arg):
+	if callable(arg):
+		func = arg
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+		return wrapper
+	else:
+		def decorator(func):
+			@functools.wraps(func)
+			def wrapper(*args, **kw):
+			return wrapper
+		return decorator
+
+@log
+def f():
+	pass
+
+f()
+
+@log('execute')
+def f():
+	pass
+
+f()
+
+
+
